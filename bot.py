11e11350598f5a31ebfdb61616f8e0ee96e9daea
Speacky081 <agrabowatschki@gmail.com>
2025-07-22 21:37:06 +0200

    sync command added

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import valheim_cog
 
 intents = dc.Intents.default()
-bot = commands.Bot("#", intents = intents)
+bot = commands.Bot("§", intents = intents)
 
 with open("TOKEN.txt", "r", encoding="utf-8") as file:
     TOKEN = file.readlines()[0]
@@ -15,6 +15,15 @@ with open("ADMINID.txt", "r", encoding="utf-8") as file:
 async def setup_hook():
     '''setup'''
     await bot.add_cog(valheim_cog.Valheim(bot))
-    await bot.tree.sync()
+
+@bot.hybrid_command(name='sync', description='Sync all commands.')
+async def cmd_sync(ctx):
+    '''syncs slash commands'''
+    if ctx.user.id == ADMIN_ID:
+        bot.tree.add_command(cmd_sync.app_command, override=True)
+        await bot.tree.sync()
+        await ctx.send('Synced successfully.', ephemeral=True)
+    else:
+        await ctx.send('You are not the owner.', ephemeral=True)
 
 bot.run(TOKEN)
