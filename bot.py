9c384ee7a8cd706e8a8fafb4c4e47ff8cf56ee2c
Speacky081 <agrabowatschki@gmail.com>
2025-07-22 21:41:09 +0200

    intents

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import valheim_cog
 
 intents = dc.Intents.default()
+intents.message_content = True
 bot = commands.Bot("§", intents = intents)
 
 with open("TOKEN.txt", "r", encoding="utf-8") as file:
