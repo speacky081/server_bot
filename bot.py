a38b2aa349a5b8198f11175916c1924cd8491663
Speacky081 <agrabowatschki@gmail.com>
2025-07-22 21:04:13 +0200

    different symbol

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import valheim_cog
 
 intents = dc.Intents.default()
-bot = commands.Bot("*", intents = intents)
+bot = commands.Bot("#", intents = intents)
 
 with open("TOKEN.txt", "r", encoding="utf-8") as file:
     TOKEN = file.readlines()[0]
